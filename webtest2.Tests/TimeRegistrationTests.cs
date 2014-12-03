3d4d8cd7a85990cd14308fd0026f537b7680c15e
Rasmus Sigsgaard <rasmus@sigsgaard.com>
2014-12-03 13:34:19 +0100

    Add initial TimeRegistration model

---
+++ webtest2.Tests/TimeRegistrationTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+using webtest2.Models;
+
+namespace webtest2.Tests
+{
+    [TestFixture]
+    public class TimeRegistrationTests
+    {
+        [Test]
+        public void ShouldAssociateProject()
+        {
+            // Arrange
+            // Act
+            var timeReg = new TimeRegistration("NewProject");
+
+            // Assert
+            Assert.That(timeReg.ProjectName, Is.EqualTo("NewProject"));
+        }
+        
+        [Test]
+        public void ShouldContainTime()
+        {
+            // Arrange
+            // Act
+            var timeReg = new TimeRegistration("")
+            {
+                Time = TimeSpan.FromHours(8.5)
+            };
+
+            // Assert
+            Assert.That(timeReg.Time, Is.EqualTo(TimeSpan.FromHours(8.5)));
+        }
+
+        [Test]
+        public void ShouldContainRegistrationTime()
+        {
+            // Arrange
+            var date = DateTimeOffset.Parse("2012-01-01");
+
+            // Act
+            var timeReg = new TimeRegistration("")
+            {
+                RegistrationDate = date
+            
+            };
+
+            // Assert
+            Assert.That(timeReg.RegistrationDate, Is.EqualTo(date));
+        }
+    }
+}
---
+++ webtest2/Models/TimeRegistration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace webtest2.Models
+{
+    public class TimeRegistration
+    {
+        public string ProjectName { get; set; }
+        public TimeSpan Time { get; set; }
+        public DateTimeOffset RegistrationDate { get; set; }
+
+        public TimeRegistration(string projectName)
+        {
+            ProjectName = projectName;
+        }
+    }
+}
