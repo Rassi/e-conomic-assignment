d8a87bac8c37cc332d2d269382b2060a1f5ebb35
Rasmus Sigsgaard <rasmus@sigsgaard.com>
2014-12-08 15:32:34 +0100

    Renames

--- TimeRegistrar.Tests/Properties/AssemblyInfo.cs
+++ TimeRegistrar.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("webtest2.Tests")]
+[assembly: AssemblyTitle("TimeRegistrar.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("webtest2.Tests")]
+[assembly: AssemblyProduct("TimeRegistrar.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
